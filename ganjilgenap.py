cbbb1368c08ead502b8959a3f39e90601f8bf1a2
Irfan-Fakhri <irfan.fakhri84@sma.belajar.id>
2026-01-19 01:11:59 +0000

    versi 1.1 kode ganjil genap

---
+++ ganjilgenap.py
@@ -0,0 +1,10 @@
+# ini adalah program mencari bilangan ganjil atau genap
+
+bil = integer(input("masukkan bilangan:"))
+
+if bil % 2 == 0:
+    print(bil, "Adalah genap")
+else: 
+    print(bil, "Adalah ganjil")
+
+print("kode ini saya buat dari codespace")
